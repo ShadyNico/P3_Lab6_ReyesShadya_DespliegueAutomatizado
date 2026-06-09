edc19902381d8ffc2d003a6f70e62fea5a602efe
ShadyNico <snreyes2@espe.edu.ec>
2026-06-09 10:39:20 -0500

    CI/CD

--- src/services/obtenerPersonajes.js
+++ src/services/obtenerPersonajes.js
@@ -1,6 +1,7 @@
 //uso js cuando yo no tengo codigo de interfaz
+const API_URL = import.meta.env.VITE_API_URL;
 export const obtenerPersonajes = async () => {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const response = await fetch(API_URL);
   const data = await response.json();
   return data.results;
 };
